a5f7e92e77d6c689f09d1c84cf5da7659cd0000a
Eric Meadow <palmercurling@android.net>
2013-10-12 19:50:16 -0400

    backend functioning

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def find_bar():
+	return render_template('index.html')
+
+app.debug=True
+if __name__ == '__main__':
+	app.run()
