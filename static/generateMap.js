6f36e1b394e53643f7dc3a472a0ce7e8a8b4d0c1
Eric Meadow <palmercurling@android.net>
2013-10-13 06:18:34 -0400

    IT LIVES

--- static/generateMap.js
+++ static/generateMap.js
@@ -1,4 +1,4 @@
-var x, y, map;
+var x, y, map, q;
 function getLocation(){
 	if (navigator.geolocation){
 		navigator.geolocation.getCurrentPosition(showPosition);
@@ -17,19 +17,41 @@ function showPosition(position){
 function generateMap(x, y){
 	var mapOptions = {
 		zoom: 8,
-		center: new google.maps.LatLng(x, y),
+		center: q = new google.maps.LatLng(x, y),
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+	var request = {
+		location: q,
+		openNow: true,
+		rankBy: google.maps.places.RankBy.DISTANCE,
+		keyword: 'bar',
+		types: ['food']
+	};
+	service = new google.maps.places.PlacesService(map);
+	service.nearbySearch(request, barCallback);
 }
 
-/**
+function barCallback(results, status) {
+	if (status == google.maps.places.PlacesServiceStatus.OK) {
+		addMarker(results[0]);
+	}
+}
+
+
 function addMarker(location) {
 	marker = new google.maps.Marker({
-		position: location,
-	   map: map
+		position: location.geometry.location,
+		map: map
 	});
-}
-**/
 
-	google.maps.event.addDomListener(window, 'load', getLocation);
+	var contentString = '<div id="content"><div id="sitenotice"></div><h1 id="firstHeading" class="firstHeading">'+location.name+'</h1> </div> </div>'
+
+	var infowindow = new google.maps.InfoWindow({
+		content: contentString
+	});
+	infowindow.open(map, marker);
+}
+
+google.maps.event.addDomListener(window, 'load', getLocation);
