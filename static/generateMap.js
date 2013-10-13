d4a17ac177155f555fa2a55155ea5f632bf102f4
Eric Meadow <palmercurling@android.net>
2013-10-13 01:54:50 -0400

    fix names cause im an idiot

--- static/generateMap.js
+++ static/generateMap.js
@@ -21,4 +21,4 @@ function generateMap(){
 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', generateMap);
